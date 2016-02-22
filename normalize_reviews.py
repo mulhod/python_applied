2f2f7f39995bfdd9fa75f66bf6fcacd0404cab13
Matthew Mulholland <mmulholland@ets.org>
2016-02-22 04:08:40 -0500

    Initial commit, first exercise

---
+++ normalize_reviews.py
@@ -0,0 +1,31 @@
+#!/bin/python
+
+def main():
+    # Read in the lines from reviews.txt and lower-case them and then
+    # write out the new lines to a new file called reviews_normalized.txt
+    inf = open('reviews.txt')
+
+    # Store the reviews in a list
+    reviews = []
+
+    # Iterate over the lines in reviews.txt and add the normalized lines
+    # to the reviews list
+    for line in inf.readlines():
+        # Fill in the code here to lower-case the line and append it to
+        # the reviews list (hint: Use lower() method to lower-case the line
+        # and the append() method to append to the reviews list)
+    inf.close()
+
+    # Write new file that contains normalized lines
+    outf = open('reviews_normalized.txt', 'w')
+    for review in reviews:
+        # Fill in the code here to write the review to the output file
+        # (hint: use the outf.write() method to write the string to the
+        # file, but remember that you'll have to add a newline, "\n", to
+        # the end of each line so that each review text is on a separate
+        # line)
+    outf.close()
+
+
+if __name__ == '__main__':
+    main()
